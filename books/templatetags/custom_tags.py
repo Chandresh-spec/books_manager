d43aeee97bbb9900e34ad4166fb8db3dc5fc53b3
Chandresh-spec <chandumoger819@gmail.com>
2025-07-22 08:22:53 +0530

    added average feature

--- books/templatetags/custom_tags.py
+++ books/templatetags/custom_tags.py
@@ -1,23 +1,17 @@
 from django import template
 
 
-
 register=template.Library()
 
 @register.filter
 
-
 def average(queryset):
-    values=[obj.stars for obj in queryset if obj.stars is not None]
+    values=[obj.stars  for obj in queryset if obj.stars  is not None]
 
     if not values:
         return "no ratings"
     
     return round(sum(values)/len(values),1)
-
-
-
-
 
 
 
