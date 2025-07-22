c0683023d6211e3df2515543f124742658c58a67
Chandresh-spec <chandumoger819@gmail.com>
2025-07-22 10:30:31 +0530

    register,login,logot feature is added

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     path('',views.helo,name='helo'),
-    path('home_page/',views.Home_page,name='home_page')
+    path('home_page/',views.Home_page,name='home_page'),
+    path('register/',views.register_view,name='register_view'),
+    path('logout/',views.logout_view,name='logout'),
+    path('login/',views.login_view,name='login')
 
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Author,Book,Review,Rating
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.forms import AuthenticationForm
 
+from .froms import RegisterForm
 # Create your views here.
 
 def helo(request):
@@ -17,6 +20,52 @@ def Home_page(request):
 
 
 
+def register_view(request):
+    if request.method=='POST':
+        form=RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return  redirect('home_page')
+
+        else:
+            return render(request,'register.html',{'form':form})
+    else:
+        form=RegisterForm()
+        return render(request,'register.html',{'form':form})
 
 
 
+
+def logout_view(request):
+    logout(request)
+    return redirect('home_page')
+
+
+def login_view(request):
+    if request.method=='POST':
+        form=AuthenticationForm(request,data=request.POST)
+
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            password=form.cleaned_data.get('password')
+
+            user=authenticate(username=username,password=password)
+
+            if  user:
+                login(request,user)
+                return redirect('home_page')
+            
+           
+           
+        
+        else:
+            return render(request,'login.html',{'form':form})
+    
+    else:
+        form=AuthenticationForm()
+        return render(request,'login.html',{'form':form})
+
+
+
+    
